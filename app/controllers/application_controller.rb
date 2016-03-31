ba2b334b76a463a6e5bbcc22a02edd466405cd26
Spinach <spinach.jp@gmail.com>
2016-03-31 14:22:26 +0000

    add auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # ログインしていない場合、ログイン画面に飛ばす。
+  before_action :authenticate_user!
+  
 end
